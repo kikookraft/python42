e35c9d3eac6e9284a6e36e5a7035da9032097e49
Tomy Besson <tobesson@student.42lyon.fr>
2026-01-26 12:25:26 +0100

    ex0

---
+++ P01/ex0/ft_garden_intro.py
@@ -0,0 +1,11 @@
+
+if __name__ == "__main__":
+    name: str = "Juan"
+    age: int = 30
+    height: float = 1.75
+    favorite_game: str = "Plant vs. Zombies"
+
+    print(f"Hello, my name is {name}.")
+    print(f"I am {age} years old.")
+    print(f"My height is {height} meters.")
+    print(f"My favorite game is {favorite_game}.")

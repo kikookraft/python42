729a526ad15b99f6537fd72d6b92af348d789af5
Tomy Besson <tobesson@student.42lyon.fr>
2026-01-27 12:16:55 +0100

    ex4

---
+++ P00/ex4/ft_water_reminder.py
@@ -0,0 +1,7 @@
+
+def ft_water_reminder():
+    wtdays: int = int(input("How much days since last water ?\n> "))
+    if wtdays > 2:
+        print("Water the plants!")
+    else:
+        print("Plants are fine")

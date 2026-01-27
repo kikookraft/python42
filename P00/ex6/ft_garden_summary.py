f399566c1265b39d201e19f31ce18b5a4d85d350
Tomy Besson <tobesson@student.42lyon.fr>
2026-01-27 12:17:10 +0100

    ex6

---
+++ P00/ex6/ft_garden_summary.py
@@ -0,0 +1,7 @@
+
+def ft_garden_summary():
+    name: str = input("Garden name: ")
+    nbpl: str = input("Number of plants: ")
+    print(f"Garden: {name}")
+    print(f"Plants: {nbpl}")
+    print("Growing well!!")

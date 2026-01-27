34acef06f3ad30ed40bfa52ca7f0118c547dfdf8
Tomy Besson <tobesson@student.42lyon.fr>
2026-01-27 14:14:47 +0100

    reversed order output

--- P00/ex5/ft_count_harvest_iterative.py
+++ P00/ex5/ft_count_harvest_iterative.py
@@ -1,5 +1,7 @@
 
 def ft_count_harvest_iterative():
     for i in range(int(input("Days until harvest: "))):
-        print("Day {i}")
+        print(f"Day {i + 1}")
     print("Harvest time!!!!")
+
+# ft_count_harvest_iterative()
--- P00/ex5/ft_count_harvest_recursive.py
+++ P00/ex5/ft_count_harvest_recursive.py
@@ -1,9 +1,13 @@
 
-def ft_count_harvest_recursive(remain: int = -1):
+def ft_count_harvest_recursive(remain: int = -1, max: int = 0):
     if remain < 0:
-        ft_count_harvest_recursive(int(input("Days until harvest: ")))
+        max = int(input("Days until harvest: "))
+        ft_count_harvest_recursive(max-1, max)
     elif remain == 0:
-        print("Harvest time!")
+        print(f"Day {max-remain}\nHarvest time!")
         return
-    print("Day {remain}")
-    ft_count_harvest_recursive(remain-1)
+    else:
+        print(f"Day {max-remain}")
+        ft_count_harvest_recursive(remain-1, max)
+
+# ft_count_harvest_recursive()

5205bc6e26aa43efe899cfa4a5831dede337e0c7
Tomy Besson <tobesson@student.42lyon.fr>
2026-01-27 12:17:13 +0100

    ex7

---
+++ P00/ex7/ft_seed_inventory.py
@@ -0,0 +1,8 @@
+
+def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
+    if unit.lower() in ["packets", "grams"]:
+        print(f"{seed_type.capitalize()} seeds: {quantity} {unit} available")
+    elif unit == "area":
+        print(f"{seed_type.capitalize()} seeds: covers {quantity} m²")
+    else:
+        print("Unknown unit type")
